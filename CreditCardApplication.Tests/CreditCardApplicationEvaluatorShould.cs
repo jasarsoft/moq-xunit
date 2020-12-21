66eb56419c40e2fc81370a8f9c2a65aa9b0ab678
Edin Jašarević <edinjasar14@gmail.com>
2020-12-21 17:08:10 +0100

    writing initial tests

---
+++ CreditCardApplication.Tests/CreditCardApplicationEvaluatorShould.cs
@@ -0,0 +1,31 @@
+using System;
+using CreditCardApplications;
+using Xunit;
+
+namespace CreditCardApplication.Tests
+{
+    public class CreditCardApplicationEvaluatorShould
+    {
+        [Fact]
+        public void AcceptHighIncomeApplications()
+        {
+            var sut = new CreditCardApplicationEvaluator();
+            var application = new CreditCardApplications.CreditCardApplication {GrossAnnualIncome = 100_000};
+
+            CreditCardApplicationDecision decision = sut.Evaluate(application);
+
+            Assert.Equal(CreditCardApplicationDecision.AutoAccepted, decision);
+        }
+
+        [Fact]
+        public void ReferYoungApplications()
+        {
+            var sut = new CreditCardApplicationEvaluator();
+            var application = new CreditCardApplications.CreditCardApplication {Age = 19};
+
+            CreditCardApplicationDecision decision = sut.Evaluate(application);
+
+            Assert.Equal(CreditCardApplicationDecision.ReferredToHuman, decision);
+        }
+    }
+}
