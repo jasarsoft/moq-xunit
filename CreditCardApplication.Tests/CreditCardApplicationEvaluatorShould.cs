9e65514e1f4986f9dc492225bf52d9a54b9e3969
Edin Jašarević <edinjasar14@gmail.com>
2021-01-02 18:23:38 +0100

    verifying a method was called

--- CreditCardApplication.Tests/CreditCardApplicationEvaluatorShould.cs
+++ CreditCardApplication.Tests/CreditCardApplicationEvaluatorShould.cs
@@ -122,5 +122,23 @@ namespace CreditCardApplication.Tests
 
             Assert.Equal(ValidationMode.Detailed, mockValidator.Object.ValidationMode);
         }
+
+        [Fact]
+        public void ValidateFrequentFlyerNumberForLowIncomeApplications()
+        {
+            var mockValidator = new Mock<IFrequentFlyerNumberValidator>();
+
+            mockValidator.Setup(x => x.ServiceInformation.License.LicenseKey).Returns("OK");
+
+            var sut = new CreditCardApplicationEvaluator(mockValidator.Object);
+            var application = new CreditCardApplications.CreditCardApplication()
+            {
+                FrequentFlyerNumber = "q"
+            };
+
+            sut.Evaluate(application);
+
+            mockValidator.Verify(p => p.IsValid(It.IsAny<string>()));
+        }
     }
 }
